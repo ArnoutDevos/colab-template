314b990808ab6a3efb875dc3081c91ce1c044607
root <arnoutdev@gmail.com>
2020-12-17 10:51:25 +0000

    Added running external python file

--- loop.py
+++ loop.py
@@ -1,8 +1,7 @@
 from tqdm import tqdm
 import time
 
-def loopbar():
-    with tqdm(total=100) as pbar:
-        for i in range(10):
-            time.sleep(0.5)
-            pbar.update(10)
+with tqdm(total=100) as pbar:
+    for i in range(10):
+        time.sleep(0.5)
+        pbar.update(10)
